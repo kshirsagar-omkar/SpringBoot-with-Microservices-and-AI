06ca8fbdd49ead83cba7cc110ef3743ebd47e412
kshirsagar-omkar <omkarkshirsagar2005@gmail.com>
2026-07-02 18:56:48 +0530

    Circular Dependency

---
+++ SpringFramework/App_06_Circular_Dependency_Problem/src/main/java/com/tca/bean/ServiceA.java
@@ -0,0 +1,12 @@
+package com.tca.bean;
+
+public class ServiceA {
+    private ServiceB serviceB;
+
+    public ServiceB getServiceB() {
+        return serviceB;
+    }
+    public void setServiceB(ServiceB serviceB) {
+        this.serviceB = serviceB;
+    }
+}
---
+++ SpringFramework/App_06_Circular_Dependency_Problem/src/main/java/com/tca/bean/ServiceB.java
@@ -0,0 +1,13 @@
+package com.tca.bean;
+
+public class ServiceB {
+    private ServiceA serviceA;
+
+    public ServiceA getServiceA() {
+        return serviceA;
+    }
+
+    public void setServiceA(ServiceA serviceA) {
+        this.serviceA = serviceA;
+    }
+}
