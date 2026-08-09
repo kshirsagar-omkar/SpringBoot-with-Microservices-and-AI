63e56ebf03ba3480e05b930c73394b66754d387a
kshirsagar-omkar <omkarkshirsagar2005@gmail.com>
2026-08-09 21:48:00 +0530

    Ignore .DS_Store and IntelliJ files

---
+++ SpringBootFramework/App_01_WelcomeApp/src/main/java/com/tca/app_01_welcomeapp/App01WelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.tca.app_01_welcomeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App01WelcomeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App01WelcomeAppApplication.class, args);
+    }
+
+}
---
+++ SpringBootFramework/App_01_WelcomeApp/src/test/java/com/tca/app_01_welcomeapp/App01WelcomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tca.app_01_welcomeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01WelcomeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
